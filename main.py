28e5ca5d377aa364fac305eecf4486f6f548d14c
Anastasiia-Bilous <ab5731141@gmail.com>
2024-09-28 21:09:23 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+name = 'Python'# This is a sample Python script.
+print(name)
+name = 'vsego zadah'# Press Shift+F10 to execute it or replace it with your code.
+name = 12# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print(name and name)
+name = 'zatraheno time'
+name = 1.5
+print(name and name)   # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
